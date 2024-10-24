25b0b64aee2a2557b7065cd1360d9b3ce17f5da3
Alexis <alexis.challande@trailofbits.com>
2024-10-24 10:15:32 +0200

    Remove test

--- test/test_init.py
+++
@@ -1,7 +0,0 @@
-import rfc3161_client
-
-
-def test_version() -> None:
-    version = getattr(rfc3161_client, "__version__", None)
-    assert version is not None
-    assert isinstance(version, str)
