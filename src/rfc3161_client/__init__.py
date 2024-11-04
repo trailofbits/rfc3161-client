8fea7a01f336ede15fd788d9e081d86296b24172
Alexis <alexis.challande@trailofbits.com>
2024-11-04 16:49:40 +0100

    Try another option
    Signed-off-by: Alexis <alexis.challande@trailofbits.com>

--- src/rfc3161_client/__init__.py
+++ src/rfc3161_client/__init__.py
@@ -1,4 +1,22 @@
 """rfc3161-client"""
+import os
+import platform
+import sys
+from pathlib import Path
+
+def add_missing_dlls():
+    print(f'ADD MISSING DIR {platform.system()}')
+    if platform.system() != "Windows":
+        return
+
+    print(f"Adding directories to DLL directory")
+    if openssl_dir := os.environ.get("OPENSSL_DIR"):
+        os.add_dll_directory(openssl_dir)
+
+    python_dir = Path(sys.executable).parent
+    os.add_dll_directory(python_dir.as_posix())
+
+add_missing_dlls()
 
 from .base import TimestampRequestBuilder, decode_timestamp_response
 from .errors import VerificationError
